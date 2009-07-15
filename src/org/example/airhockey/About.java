dec713edaecd76abc5fa2eec0b64f78849b69ea0
naoya_t <naoya_t@users.sourceforge.jp>
2009-07-15 12:56:58 +0900

    相手がすこしはまともになった

---
+++ src/org/example/airhockey/About.java
@@ -0,0 +1,12 @@
+package org.example.airhockey;
+
+import android.app.Activity;
+import android.os.*;
+
+public class About extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
